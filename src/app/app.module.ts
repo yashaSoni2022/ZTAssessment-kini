f595af7cb55e9ca9d5b8c0e04c3f488e1ca1e982
ivicavukosavljevic <ivicavuk1212@gmail.com>
2023-02-16 19:07:49 +0100

    File manipulation screens & text manipulation fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { OrderinteractionComponent } from './components/orderinteraction/orderinteraction.component';
 import { AdminHoverCardComponent } from './components/admin-hover-card/admin-hover-card.component';
+import { FileManipulationComponent } from './components/file-manipulation/file-manipulation.component';
 
 @NgModule({
   declarations: [
@@ -16,8 +18,19 @@ import { AdminHoverCardComponent } from './components/admin-hover-card/admin-hov
     SideBarComponent,
     AdminHoverCardComponent,
     OrderinteractionComponent,
+    FileManipulationComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'file-manipulation',
+        component: FileManipulationComponent,
+      },
+      { path: 'order-interaction', component: OrderinteractionComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/file-manipulation/file-manipulation.component.ts
@@ -0,0 +1,72 @@
+import { Component, Input } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-file-manipulation',
+  templateUrl: './file-manipulation.component.html',
+  styleUrls: ['./file-manipulation.component.css'],
+})
+export class FileManipulationComponent {
+  savebtn: boolean = true;
+  savebtn1: boolean = false;
+  Condition1: boolean = true;
+  autosave: boolean = true;
+  bottomSymbol: boolean = true;
+  delete1: boolean = true;
+  delete2: boolean = true;
+  delete3: boolean = true;
+  choicebox: boolean = true;
+  delete4: boolean = false;
+  Condition2: boolean = false;
+  Condition3: boolean = false;
+  choice1: boolean = true;
+  choice2: boolean = true;
+  choice3: boolean = true;
+  choice4: boolean = false;
+  styleBold: boolean = false;
+  styleItalic: boolean = false;
+  styleUnderline: boolean = false;
+  styleFour: boolean = false;
+  styleFive: boolean = false;
+  value: string = '';
+  getValue(val: string) {
+    this.value = val;
+  }
+
+  screen2() {
+    this.Condition1 = false;
+    this.Condition2 = true;
+    this.autosave = false;
+    this.bottomSymbol = false;
+  }
+
+  screen3() {
+    this.Condition2 = false;
+    this.Condition3 = true;
+  }
+
+  dismiss() {
+    this.bottomSymbol = false;
+  }
+
+  dismissbox() {
+    this.Condition1 = false;
+    this.autosave = false;
+  }
+
+  boldText() {
+    if (this.value) this.styleBold = !this.styleBold;
+  }
+
+  italicText() {
+    if (this.value) this.styleItalic = !this.styleItalic;
+  }
+
+  underlineText() {
+    if (this.value) this.styleUnderline = !this.styleUnderline;
+  }
+
+  browseFiles() {
+    console.log('Browse files');
+  }
+}

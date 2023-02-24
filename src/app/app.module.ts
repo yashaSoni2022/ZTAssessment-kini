9eac1b91c0d351839612eafce39a2416c1fe328c
abhi-procreator <hi2abhi.shrm@gmail.com>
2023-02-24 21:13:15 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { CardBoxComponent } from './components/card-box/card-box.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ChoiceBoxComponent } from './components/choice-box/choice-box.component';
+
 
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SideBarComponent, CardBoxComponent],
+  declarations: [AppComponent, HeaderComponent, SideBarComponent, CardBoxComponent, ChoiceBoxComponent],
   imports: [BrowserModule, AppRoutingModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent],

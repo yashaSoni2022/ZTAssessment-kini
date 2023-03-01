b4d4a2fae5b5340f47e94793723f1a7ba27132ff
abhi-procreator <hi2abhi.shrm@gmail.com>
2023-03-01 23:30:05 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
@@ -17,41 +17,31 @@ import { Graphicalinteraction12Component } from './components/graphicalinteracti
 import { MatchInteractionComponent } from './components/match-interaction/match-interaction.component';
 import { Ctm001002Component } from './components/ctm001002/ctm001002.component';
 import { ExtTextInteractionComponent } from './components/ext-text-interaction/ext-text-interaction.component';
+import { ChoiceBoxComponent } from "./components/choice-box/choice-box.component";
+import { CardBoxComponent } from './components/card-box/card-box.component';
+import { Choiceintraction002Component } from './components/choiceintraction002/choiceintraction002.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SideBarComponent,
-    AdminHoverCardComponent,
-    OrderinteractionComponent,
-    FileManipulationComponent,
-    GraphicalInteractionComponent,
-    HottextInteractionComponent,
-    Graphicalinteraction12Component,
-    MatchInteractionComponent,
-    Ctm001002Component,
-    ExtTextInteractionComponent
-  ],
-  // imports: [
-  //   BrowserModule,
-  //   AppRoutingModule,
-  //   DragDropModule,
-  //   FormsModule,
-  //   RouterModule.forRoot([
-  //     { path: 'file-manipulation', component: FileManipulationComponent },
-  //     { path: 'order-interaction', component: OrderinteractionComponent },
-  //     {
-  //       path: 'graphical-interaction',
-  //       component: GraphicalInteractionComponent,
-  //     },
-  //     { path: 'hottext-interaction', component: HottextInteractionComponent },
-  //   ]),
-  // ],
-
-  imports: [BrowserModule, AppRoutingModule, FormsModule, DragDropModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        SideBarComponent,
+        AdminHoverCardComponent,
+        OrderinteractionComponent,
+        FileManipulationComponent,
+        GraphicalInteractionComponent,
+        HottextInteractionComponent,
+        Graphicalinteraction12Component,
+        MatchInteractionComponent,
+        Ctm001002Component,
+        ExtTextInteractionComponent,
+        ChoiceBoxComponent,
+        CardBoxComponent,
+        Choiceintraction002Component
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, FormsModule, DragDropModule, ReactiveFormsModule]
 })
 export class AppModule {}
